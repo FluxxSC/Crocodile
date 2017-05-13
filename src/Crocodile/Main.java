298d1794ec5e612a354432b4ac06e853bcd40211
Rune <fluxxsc@gmail.com>
2017-05-13 23:41:08 +0200

    basic generator

---
+++ src/Crocodile/Main.java
@@ -0,0 +1,25 @@
+package Crocodile;
+
+public class Main {
+	
+	private Map map;
+	
+	public Main() {
+		map = new Map(30, 30, 20);
+		System.out.println(map);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
